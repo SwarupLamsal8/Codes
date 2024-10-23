3cdcaa7be9a93c81ab7bfa7cedd87e7c71bf15f2
Swarup lamsal <swarup99887@gmail.com>
2024-10-23 12:33:12 +0545

    Just An Edit

--- Web Development/5 React/Project/my-react-app/src/App.jsx
+++ Web Development/5 React/Project/my-react-app/src/App.jsx
@@ -1,9 +1,10 @@
-import DigitalClock from './DigitalClock.jsx'
+import StopWatch from './StopWatch.jsx'
 
 function App() {
+
   return (
     <>
-      <DigitalClock />
+      <StopWatch/>
     </>
   );  
 }
